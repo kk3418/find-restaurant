5c0d05e596c03a14b720345e0766a6e907ac9c8d
kevin <kevin345618@outlook.com>
2021-03-26 17:36:19 +0800

    build a sample

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,25 +1,40 @@
 <template>
-  <div id="map"></div>
+  <GoogleMap
+    api-key="AIzaSyArwUk0co_Ur3wviOtN0UI9_am-dN03hEA"
+    style="width: 100%; height: 100%; top:-5vh;"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
   <div id="list">
     <List :count="count" />
     <button @click="onClick()">search</button>
   </div>
 </template>
 <script>
+import { GoogleMap, Marker } from "vue3-google-map";
 import List from "./List.vue";
 
 export default {
   name: "Map",
   components: {
     List,
+    GoogleMap,
+    Marker,
   },
   data() {
     return {
-      map: null,
-      lat: 25.04,
-      lng: 121.512,
       count: 1,
     };
+  },
+  setup() {
+    // no need to use mounted outside setup, unse onMount here instead
+    const center = {
+      lat: 25.04,
+      lng: 121.512,
+    };
+    return { center };
   },
   methods: {
     onClick() {
@@ -30,11 +45,6 @@ export default {
 };
 </script>
 <style scoped>
-#map {
-  top: -5vh;
-  height: 100%;
-  width: 100%;
-}
 #list {
   position: absolute;
   width: 20vw;
